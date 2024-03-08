d4004b567d4dc44bccecb78470c706213ef3f34a
SomeBodyM <151923587+SomeBodyM@users.noreply.github.com>
2024-03-08 18:02:01 +0200

    a

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import {Counter} from "entities/Counter";
 
+const a = 0;
 const AboutPage = () => {
     const { t } = useTranslation('about')
     return (
